431c663e5a63cddcbd0fb9e931e8e37e0d1d0edc
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2021-10-06 14:24:14 +0700

    client

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use("/user", userRouter);
+app.use('/api/posts', postRoutes);
+app.use('/api/user', userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://amazona:8365598a@cluster0.fdtaq.mongodb.net/memories?authSource=admin&replicaSet=atlas-55bbid-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 const PORT = process.env.PORT|| 5000;
