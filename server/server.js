a514a0bd53b2cdd297191a27a8c54ca01982d866
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2021-10-06 12:20:34 +0700

    'backend'

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import path from 'path';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
@@ -19,6 +20,9 @@ app.use("/user", userRouter);
 const CONNECTION_URL = 'mongodb+srv://amazona:8365598a@cluster0.fdtaq.mongodb.net/memories?authSource=admin&replicaSet=atlas-55bbid-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 const PORT = process.env.PORT|| 5000;
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html')))
 mongoose.connect(CONNECTION_URL, { })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
